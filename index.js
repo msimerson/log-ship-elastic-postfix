faea210be3e226ce8280bf239fd2a3106c54fb9b
Matt Simerson <matt@tnpi.net>
2015-07-25 14:49:00 -0700

    config loading, load parser & elasticsearch

--- index.js
+++ index.js
@@ -6,12 +6,24 @@ var path      = require('path');
 var ini       = require('ini');
 
 function PostfixToElastic (etcDir) {
-	this.cfg   = this.loadConfig(etcDir);
-	this.spool = this.validateSpoolDir();
+	this.cfg    = this.loadConfig(etcDir);
+	this.spool  = this.cfg.main.spool || '/var/spool/log-ship';
+	this.validateSpoolDir();
+
+	this.parser = require(this.cfg.parser.module);
+
+	var esm = require(this.cfg.elastic.module);
+	this.eshosts = this.cfg.elastic.hosts.split(/[, ]+/);
+	console.log(this.eshosts);
+	this.elastic = new esm.Client({
+		hosts: this.eshosts,
+		log: 'trace',
+	});
 
 
 
 }
+
 
 PostfixToElastic.prototype.loadConfig = function(etcDir) {
 	var file = 'log-ship-elasticsearch-postfix.ini';
@@ -21,27 +33,72 @@ PostfixToElastic.prototype.loadConfig = function(etcDir) {
 		candidates.push(path.resolve('/etc', file));
 	}
 
+	// first one that is readable wins
 	for (var i = 0; i < candidates.length; i++) {
 		var filePath = candidates[i];
-		if (!fs.existsSync(filePath)) continue;
-		return ini.parse(fs.readFileSync(filePath, 'utf-8'));
-	};
+		try {
+			var data = fs.readFileSync(filePath, 'utf-8');
+			return ini.parse(data);
+		}
+		catch (ignore) {}
+	}
 };
 
 PostfixToElastic.prototype.validateSpoolDir = function(done) {
-	var dir = this.cfg.main.spool || '/var/spool/log-ship';
 
-	fs.stat(dir, function (err, stat) {
-		if (err && err.code === 'ENOENT') {
-			// TODO: make this recursive (stat parent until one exists)
-			console.log('mkdir: ' + dir);
-			fs.mkdir(dir, function (err) {
-				if (err) return done(err);
-				done(err, true);
-			});
+	if (!done) {
+		if (this.isDirectory(this.spool) && this.isWritable(this.spool)) {
+			return true;
 		}
-		// body...
-	})
+		return false;
+	}
+
+	this.isDirectory(this.spool, function (err) {
+		if (err) return done(err);
+		this.isWritable(this.spool, function (err) {
+			if (err) return done(err);
+			done(err, true);
+		});
+	}.bind(this));
+};
+
+PostfixToElastic.prototype.isDirectory = function(dir, done) {
+	if (!done) return fs.statSync(dir).isDirectory();
+
+	fs.stat(dir, function (err, stats) {
+		if (err) {
+			if (err.code === 'ENOENT') {
+				// TODO: make this recursive
+				console.log('mkdir: ' + dir);
+				fs.mkdir(dir, function (err) {
+					if (err) return done(err);
+					done(err, true);
+				});
+			}
+			return done(err);
+		}
+		return done(err, stats.isDirectory());
+	});
+};
+
+PostfixToElastic.prototype.isWritable = function(dir, done) {
+	if (!done) {
+		try {
+			fs.accessSync(dir, fs.W_OK);
+		}
+		catch (e) {
+			return false;
+		}
+		return true;
+	}
+
+	fs.access(dir, fs.W_OK, function (err) {
+		if (err) {
+			console.error('ERROR: spool dir is not writable: ' + err.code);
+			return done(err);
+		}
+		done(err, true);
+	});
 };
 
 module.exports = {
--- test/config.js
+++ test/config.js
@@ -1,43 +1,105 @@
 'use strict';
 
 var assert  = require('assert');
-var fs      = require('fs')
+var fs      = require('fs');
+var path    = require('path');
 
 var shipper = require('../index');
 
 describe('log-ship-elasticsearch-postfix', function () {
+    var Ship = shipper.createShipper('./test');
+
     describe('config', function () {
-        it('finds a log-ship-elasticsearch-postfix.ini', function (done) {
-            var Ship = shipper.createShipper('./test');
-            assert.ok(Ship)
-            // console.log(Ship.cfg);
+        it('finds a log-ship-elasticsearch-postfix.ini', function (done) {        
+            assert.ok(Ship);
             done();
         });
 
         it('config has required sections', function (done) {
-            var cfg = shipper.createShipper('./test').cfg;
             // console.log(cfg);
             ['main', 'elastic', 'parser', 'reader'].forEach(function (s) {
-                assert.ok(cfg[s]);
+                assert.ok(Ship.cfg[s]);
             });
             done();
         });
 
-        it('determines the spool dir', function (done) {
-            var cfg = shipper.createShipper('./test').cfg;
-            assert.ok(cfg.main.spool);
+        it('spool dir is defined', function (done) {
+            assert.ok(Ship.cfg.main.spool);
             done();
         });
 
         it('spool dir is writable', function (done) {
-            var cfg = shipper.createShipper('./test').cfg;
-            fs.access(cfg.main.spool, fs.W_OK, function(err) {
+            fs.access(Ship.cfg.main.spool, fs.W_OK, function(err) {
                 assert.ifError(err);
                 done();
             });
         });
 
-        it('throws if spool dir is not writable', function (done) {
+        it('errs if spool dir is not writable', function (done) {
+            var errShip = shipper.createShipper('./test');
+            errShip.spool = path.resolve('./test', 'spool.nowrite');
+            // console.log(errShip);
+            errShip.validateSpoolDir(function (err) {
+                assert.equal(err.code, 'EACCES');
+                done();
+            });
+        });
+    });
+
+    describe('parser', function () {
+        it('loads the specified parser', function (done) {
+            assert.ok(Ship.parser);
+            done();
+        });
+    });
+
+    describe('elasticsearch', function () {
+        it('loads the specified elasticsearch module', function (done) {
+            assert.ok(Ship.elastic);
+            done();
+        });
+
+        var hostName = require('os').hostname();
+        if (/travis|tworker|dev-test/.test(hostName)) {
+
+
+            it('connects to configured ES host', function (done) {
+
+                Ship.elastic.ping({
+                    // ping usually has a 3000ms timeout 
+                    // requestTimeout: Infinity,
+                }, function (error) {
+                    if (error) {
+                       return done('elasticsearch cluster is down!');
+                    }
+                    done(error, 'All is well');
+                });
+            });
+        }
+    });
+
+    describe('fs utilities', function () {
+        it('isDirectory reports true for dir', function (done) {
+            assert.equal(
+                Ship.isDirectory(path.resolve('./test', 'spool')), true);
+            done();
+        });
+
+        it('isDirectory reports false for file', function (done) {
+            var spoolFile = path.resolve('./test', 'spool.file');
+            assert.equal(Ship.isDirectory(spoolFile), false);
+            done();
+        });
+
+        it('isWritable reports true for writable dir', function (done) {
+            var spoolDir = path.resolve('./test', 'spool');
+            assert.equal(Ship.isWritable(spoolDir), true);
+            done();
+        });
+
+        it('isWritable reports false for non-writable dir', function (done) {
+            var spoolDir = path.resolve('./test', 'spool.nowrite');
+            assert.equal(Ship.isWritable(spoolDir), false);
             done();
         });
     });
