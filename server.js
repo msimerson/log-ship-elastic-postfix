b63b15d969f44b6f912982552caf3bb449b356e6
Matt Simerson <matt@tnpi.net>
2015-08-04 14:55:23 -0700

    shut down the process after 3 failed attempts
    (let process manager / watcher restart)

--- server.js
+++ server.js
@@ -6,33 +6,6 @@ var util     = require('util');
 var logship  = require('./lib/logship');
 var shipper  = logship.createShipper('./');
 
-var shutdown = function () {
-    console.log('starting graceful shutdown');
-
-    process.env.WANTS_SHUTDOWN=1;
-
-    if (!shipper.elasticAlive) process.exit();
-    if (!shipper.queueActive) process.exit();
-
-    var maxWait = setTimeout(function () {
-        // if things haven't shut down in 10s, die anyway
-        process.exit();
-    }, 10 * 1000);
-
-    function waitForQueue () {
-        if (shipper.queueActive) {
-            console.log('queue is active');
-            setTimeout(function () {
-                waitForQueue();
-            }, 1 * 1000);
-            return;
-        }
-
-        process.exit();
-    }
-    waitForQueue();
-};
-
 /*
 process.on('SIGHUP', function () {
     console.log('SIGHUP: reloading config');
@@ -40,10 +13,10 @@ process.on('SIGHUP', function () {
 */
 process.on('SIGINT', function() {     // Control-C
     console.log('\nSIGINT received (Ctrl-C)');
-    shutdown();
+    shipper.shutdown();
 });
 
 process.on('SIGTERM', function () {   // kill $PID
     console.log('\nSIGTERM received');
-    shutdown();
+    shipper.shutdown();
 });
